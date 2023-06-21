de028b3aabbf5277961fdafef52481584c051107
Michael Duren <michaeld@michaelduren.com>
2023-06-21 16:29:51 -0500

    Added DB Context Added DB Context, Coupon Database, etc. Updated API endpoints to use DB Context instead of static data.

--- MagicVilla_CouponAPI/Program.cs
+++ MagicVilla_CouponAPI/Program.cs
@@ -31,10 +31,12 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/api/coupon", () =>
+app.MapGet("/hello", () => "HELLO");
+
+app.MapGet("/api/coupon", (AppDbContext context) =>
     {
         ApiResponse response = new();
-        response.Result = CouponStore.couponList;
+        response.Result = context.Coupons;
         response.IsSuccess = true;
         response.StatusCode = HttpStatusCode.OK;
 
@@ -43,10 +45,10 @@ app.MapGet("/api/coupon", () =>
     .Produces<ApiResponse>();
 
 app.MapGet("/api/coupon/{id:int}",
-        (int id) =>
+        async (AppDbContext context, int id) =>
         {
             ApiResponse response = new();
-            response.Result = CouponStore.couponList.FirstOrDefault(u => u.Id == id);
+            response.Result = await context.Coupons.FirstOrDefaultAsync(u => u.Id == id);
             response.IsSuccess = true;
             response.StatusCode = HttpStatusCode.OK;
 
@@ -56,7 +58,8 @@ app.MapGet("/api/coupon/{id:int}",
     .Produces<ApiResponse>(200);
 
 app.MapPost("/api/coupon",
-        async (IValidator<CouponCreateDto> validator, IMapper mapper, [FromBody] CouponCreateDto couponCDto) =>
+        async (AppDbContext context, IValidator<CouponCreateDto> validator, IMapper mapper,
+            [FromBody] CouponCreateDto couponCDto) =>
         {
             ApiResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest };
 
@@ -69,8 +72,8 @@ app.MapPost("/api/coupon",
 
             var coupon = mapper.Map<Coupon>(couponCDto);
 
-            coupon.Id = CouponStore.couponList.MaxBy(u => u.Id)!.Id + 1;
-            CouponStore.couponList.Add(coupon);
+            context.Coupons.Add(coupon);
+            await context.SaveChangesAsync();
             var couponDto = mapper.Map<CouponDto>(coupon);
 
             response.Result = couponCDto;
@@ -82,12 +85,13 @@ app.MapPost("/api/coupon",
     .WithName("CreateCoupon").Accepts<CouponCreateDto>("application/json")
     .Produces<ApiResponse>(201).Produces(400);
 
-app.MapPut("/api/coupon/{id:int}", async (IValidator<CouponUpdateDto> validator, IMapper mapper, int id,
+app.MapPut("/api/coupon/{id:int}", async (AppDbContext context, IValidator<CouponUpdateDto> validator, IMapper mapper,
+        int id,
         [FromBody] CouponUpdateDto couponUpdateDto) =>
     {
-        ApiResponse response = new() { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest };
-        var couponToUpdate = CouponStore.couponList.FirstOrDefault(u => u.Id == id);
-        if (couponToUpdate is null) return Results.BadRequest(response);
+        ApiResponse response = new()
+            { IsSuccess = false, StatusCode = HttpStatusCode.BadRequest, ErrorMessages = { "Invalid Id For Coupon" } };
+        if (!context.Coupons.Any(u => u.Id == id)) return Results.BadRequest(response);
 
         var validation = await validator.ValidateAsync(couponUpdateDto);
 
@@ -98,8 +102,8 @@ app.MapPut("/api/coupon/{id:int}", async (IValidator<CouponUpdateDto> validator,
         }
 
         var coupon = mapper.Map<Coupon>(couponUpdateDto);
-        CouponStore.couponList.Remove(couponToUpdate);
-        CouponStore.couponList.Add(coupon);
+        context.Coupons.Update(coupon);
+        await context.SaveChangesAsync();
 
         var couponDto = mapper.Map<CouponDto>(coupon);
         response.Result = couponDto;
@@ -112,24 +116,26 @@ app.MapPut("/api/coupon/{id:int}", async (IValidator<CouponUpdateDto> validator,
     .Produces<ApiResponse>(200).Produces(400);
 
 
-app.MapDelete("/api/coupon/{id:int}", (int id) =>
+app.MapDelete("/api/coupon/{id:int}", async (AppDbContext context, int id) =>
     {
         ApiResponse response = new()
         {
             IsSuccess = false, StatusCode = HttpStatusCode.BadRequest,
             ErrorMessages = new List<string> { "Error, Invalid ID" }
         };
-        var couponToRemove = CouponStore.couponList.FirstOrDefault(u => u.Id == id);
+        var couponToRemove = await context.Coupons.FirstOrDefaultAsync(u => u.Id == id);
         if (couponToRemove is null) return Results.BadRequest(response);
 
-        CouponStore.couponList.Remove(couponToRemove);
+        context.Coupons.Remove(couponToRemove);
+        await context.SaveChangesAsync();
 
         response.Result = null;
         response.IsSuccess = true;
         response.StatusCode = HttpStatusCode.NoContent;
+        response.ErrorMessages = null;
         return Results.Ok(response);
     })
-    .WithName("UpdateCoupon").Accepts<CouponUpdateDto>("application/json")
+    .WithName("DeleteCoupon").Accepts<int>("application/json")
     .Produces<ApiResponse>(200).Produces(400);
 
 app.UseHttpsRedirection();
--- MagicVilla_CouponAPI/Validation/CouponCreateValidation.cs
+++ MagicVilla_CouponAPI/Validation/CouponCreateValidation.cs
@@ -6,15 +6,18 @@ namespace MagicVilla_CouponAPI.Validation;
 
 public class CouponCreateValidation : AbstractValidator<CouponCreateDto>
 {
-    public CouponCreateValidation()
+    private readonly AppDbContext _context;
+
+    public CouponCreateValidation(AppDbContext context)
     {
+        _context = context;
         RuleFor(model => model.Name).NotEmpty().Must(UniqueName).WithMessage("This Coupon Name already exists.");
         RuleFor(model => model.Percent).InclusiveBetween(1, 100);
     }
 
     private bool UniqueName(CouponCreateDto couponCreateDto, string name)
     {
-        var foundName = CouponStore.couponList.FirstOrDefault(x => x.Name == name);
+        var foundName = _context.Coupons.FirstOrDefault(x => x.Name == name);
         return foundName is null ? true : false;
     }
 }
